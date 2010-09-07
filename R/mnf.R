32a09a8671c447468cf2e0b8762ffe4df128e476
Philippe Serhal <philippe.serhal@umontreal.ca>
2010-09-07 11:33:23 -0400

    Support for AffyBatch objects in addition to RGList objects; various further generalizations and modifications.

--- R/mnf.R
+++ R/mnf.R
@@ -1,60 +1,121 @@
-# vim: set filetype=r:
+# vim:set filetype=r:
 
+require (affy)
 dyn.load (paste ("src/mnf_funcs", .Platform$dynlib.ext, sep = ""))
 
-normalize.mnf <- function (object, verbose = TRUE, ...) {
-    if (class (object) == "RGList") {
-        nobject <- object
-
-        if (verbose)
-            cat ("Normalizing green channel...\n")
-        nobject$G <- normalizeChannel (object$G, object$genes, verbose = verbose, ...)
-        if (verbose)
-            cat ("Normalizing red channel...\n")
-        nobject$R <- normalizeChannel (object$R, object$genes, verbose = verbose, ...)
-        if (verbose)
-            cat ("All processing complete.\n")
-
-        nobject
-    } else {
-        stop ("'object' must be of class RGList")
+normalize.mnf <- function (object, channel = "both", interest = "genome", bias = "grid", features.i = NULL, features.b = NULL, probe.filter = function (v) rep (TRUE, length (v)), verbose = TRUE, ...) {
+    if (is.null (features.i)) {
+        features.i <- switch (interest,
+            genome = order (RG$genes$PROBE_ID), # This is MP_-specific
+            none = NULL,
+            stop ("Need one of 'interest' or 'features.i'")
+        )
     }
 
-    #object <- MA.RG (object)
-    #asExprSet (object, idColumn = "PROBE_ID")
+    if (is.null (features.b)) {
+        features.b <- switch (bias,
+            grid = RG$genes[, c ("X", "Y")],
+            length = order (length (RG$genes[, "sequence"]), sample (1:length (RG))),
+            none = NULL,
+            stop ("Need one of 'bias' or 'features.b'")
+        )
+    }
+
+    return (switch (class (object),
+        RGList = normalize.mnf.RGList (object, channel, features.i, features.b, probe.filter, verbose, ...),
+        AffyBatch = normalize.mnf.AffyBatch (object, channel, features.i, features.b, probe.filter, verbose, ...),
+        stop ("'object' must be of class RGList or AffyBatch")
+    ))
 }
 
-normalizeChannel <- function (channel, genes, ki = 2, kb = 20, summaryStatistic.i = "mean", summaryStatistic.b = "median", verbose = TRUE) {
-    if (class (channel) == "matrix") {
+normalize.mnf.RGList <- function (rg, channel, features.i, features.b, probe.filter, verbose, ...) {
+    #probes <- rg$genes$Status == "Probe"
+    probes <- probe.filter (rg$genes)
+
+    for (e in 1:ncol (rg)) {
+        if (verbose)
+            cat ("Normalizing array ", e, "...\n", sep =  "")
+
+        if (channel %in% c ("green", "both")) {
+            if (verbose)
+                cat ("  Green channel...\n")
+            rg$G[probes] <- normalizeChannel (rg$G[probes,e], features.i = subset (features.i, probes), features.b = subset (features.b, probes), verbose = verbose, ...)
+        }
+
+        if (channel %in% c ("red", "both")) {
+            if (verbose)
+                cat ("  Red channel...\n")
+            rg$R[probes] <- normalizeChannel (rg$R[probes,e], features.i = subset (features.i, probes), features.b = subset (features.b, probes), verbose = verbose, ...)
+        }
+    }
+
+    if (verbose)
+        cat ("All processing complete.\n")
+    rg
+    #rg <- MA.RG (rg)
+    #asExprSet (rg, idColumn = "PROBE_ID")
+}
+
+normalize.mnf.AffyBatch <- function (batch, channel, features.i, features.b, probe.filter, verbose, ...) {
+    npm <- matrix (nrow = nrow (pm (batch)), ncol = ncol (pm (batch)))
+    nmm <- matrix (nrow = nrow (mm (batch)), ncol = ncol (mm (batch)))
+
+    for (e in 1:length (batch)) {
+        if (verbose)
+            cat ("Normalizing array ", e, "...\n", sep =  "")
+
+        if (channel %in% c ("pm", "both")) {
+            if (verbose)
+                cat ("  PM...\n")
+            npm[,e] <- normalizeChannel (pm (batch[,e]), features.i = features.i, features.b = features.b, verbose = verbose, ...)
+        }
+
+        if (channel %in% c ("mm", "both")) {
+            if (verbose)
+                cat ("  MM...\n")
+            nmm[,e] <- normalizeChannel (mm (batch[,e]), features.i = features.i, features.b = features.b, verbose = verbose, ...)
+        }
+    }
+
+    if (channel %in% c ("pm", "both"))
+        pm (batch) <- npm
+    if (channel %in% c ("mm", "both"))
+        mm (batch) <- nmm
+
+    if (verbose)
+        cat ("All processing complete.\n")
+    batch
+}
+
+normalizeChannel <- function (channel, features.i, features.b, ki = 2, kb = 20, summaryStatistic.i = "mean", summaryStatistic.b = "median", verbose = TRUE) {
+    if (is.vector (channel) || (is.matrix (channel) && ncol (channel) == 1)) {
         si <- rowify (summaryStatistic.i)
         sb <- rowify (summaryStatistic.b)
-        probes <- genes$Status == "Probe"
-        channel.probes <- channel[probes]
 
         if (verbose)
-            cat ("\tComputing residuals...\n")
-        res <- residuals.mnf (channel.probes, order (genes$PROBE_ID[probes]), ki, si)
+            cat ("    Computing residuals...\n")
+        res <- residuals.mnf (channel, features.i, ki, si)
 
         if (verbose)
-            cat ("\tLocating grid neighbours...\n")
-        neighbours <- knn.mnf (genes[probes, c ("X", "Y")], kb)
+            cat ("    Locating neighbours in bias space...\n")
+        neighbours <- knn.mnf (features.b, kb)
 
         if (verbose)
-            cat ("\tCorrecting values...\n")
-        nchannel <- channel
+            cat ("    Correcting values...\n")
         ncells <- as.integer (prod (dim (neighbours)))
         mappedRes <- .C ("map_values", ncells, as.integer (neighbours), as.integer (res), as.integer (vector (length = ncells)), NAOK = TRUE, DUP = FALSE) [[4]]
-        nchannel[probes] <- channel.probes - sb (matrix (mappedRes, nrow = length (res)))
-
-        nchannel
+        channel - sb (matrix (mappedRes, nrow = length (res)))
     } else {
-        stop ("'channel' must be a matrix")
+        stop ("'channel' must be a vector or a 1-column matrix")
     }
 }
 
 rowify <- function (fun) switch (fun, mean = rowMeans, median = rowMedians, min = rowMin, max = rowMax)
 
 residuals.mnf <- function (channel, pos, k, sumStat) {
+    if (is.null (pos))
+        return (channel)
+
     neighbours <- knn.mnf (pos, k)
     ncells <- as.integer (prod (dim (neighbours)))
     mappedVals <- .C ("map_values", ncells, as.integer (neighbours), as.integer (channel), as.integer (vector (length = ncells)), NAOK = TRUE, DUP = FALSE) [[4]]
@@ -80,4 +141,45 @@ knn.mnf.2D <- function (x, y, k) {
     matrix (.C ("grid_neighbours", as.integer (n), as.integer (x), as.integer (y), as.integer (k), as.integer (vector (length = n * k)), NAOK = TRUE, DUP = FALSE) [[5]], nrow = n, ncol = k, byrow = TRUE)
 }
 
-pcor <- function (v) { n <- length (v); cor (v[1:(n - 1)], v[2:n]) }
+pcor <- function (v, d = 1) { n <- length (v); cor (v[1:(n - d)], v[(1 + d):n]) }
+
+pcors <- function (v, ds = 0:10) {
+    cors <- vector (length = length (ds))
+    for (i in 1:length (ds))
+        cors[i] <- pcor (v, ds[i])
+    cors
+}
+
+plotAutoCor <- function (v, ds = 0:10, ...) {
+    plot (pcors (v, ds) ~ ds, type = "b", xlab = "Lag", ylab = "Genomic autocorrelation", ylim = c (0, 1), ...)
+}
+
+ssd.probeset.intra <- function (indices, values, sample.size = 200) {
+    indices <- sample (indices, sample.size)
+    ssd <- 0
+    for (ps in indices) {
+        np <- length (ps)
+        for (i in 1:(np - 1)) {
+            p <- ps[i]
+            o <- ps[(i + 1):np]
+            ssd <- ssd + sum ((values[p] - values[o]) ^ 2)
+        }
+    }
+    sqrt (ssd)
+}
+
+ssd.probeset.inter <- function (indices, values, sample.size = 200) {
+    indices <- sample (indices, sample.size)
+    ssd <- 0
+    for (i in 1:(sample.size - 1)) {
+        ps <- indices[[i]]
+        np <- length (ps)
+        for (p in ps)
+            for (o in indices[(i + 1):sample.size])
+                ssd <- ssd + sum ((values[p] - values[o]) ^ 2)
+    }
+    sqrt (ssd)
+}
+
+ssd.probeset.ratio <- function (indices, values, sample.size.intra = 200, sample.size.inter = 200)
+    ssd.probeset.intra (indices, values, sample.size.intra) / ssd.probeset.inter (indices, values, sample.size.inter)
