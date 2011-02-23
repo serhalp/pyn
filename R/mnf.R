fdc0af057b4afb369d37b39f2c98f835bb92b13f
Philippe Serhal <philippe.serhal@umontreal.ca>
2011-02-23 12:19:13 -0500

    Style fix: all lines now fit into 80 columns (with some leniency), cut off in a consistent way (although I'm not convinced it's a great system).

--- R/mnf.R
+++ R/mnf.R
@@ -4,7 +4,9 @@ library (affy)
 # FIXME: Path here should be relative to package root.
 dyn.load (paste ("~/workspace/rmnf/src/mnf_funcs", .Platform$dynlib.ext, sep = ""))
 
-mnf <- function (batch, samples, interest = "probeset", bias = "grid", features.i = NULL, features.b = NULL, verbose = TRUE, ...) {
+mnf <- function (batch, samples, interest = "probeset", bias = "grid",
+    features.i = NULL, features.b = NULL, verbose = TRUE, ...)
+{
     if (is.null (features.i)) {
         features.i <- switch (interest,
             probeset = NULL, # FIXME: This is not very expressive...
@@ -32,7 +34,11 @@ mnf <- function (batch, samples, interest = "probeset", bias = "grid", features.
         na <- length (a)
         nb <- length (b)
         df <- na + nb - 2
-        tstat <- ((mean (a) - mean (b)) / (sqrt (((na - 1) * var (a) + (nb - 1) * var (b)) / df) * sqrt (1 / na + 1 / nb)))
+        tstat <- ((mean (a) - mean (b)) /
+            (sqrt (((na - 1) * var (a) + (nb - 1) * var (b)) / df) *
+                sqrt (1 / na + 1 / nb)
+            )
+        )
         return (2 * pt (-abs (tstat), df))
     }
 
@@ -43,13 +49,15 @@ mnf <- function (batch, samples, interest = "probeset", bias = "grid", features.
         arrays.second <- which (samples == pair[2])
 
         # Compute p-value of two-sample t-test, for each gene
-        p.values <- sapply (seq (num.genes),
-            function (g) t.test.p.value (exprs.genes[g,arrays.first], exprs.genes[g,arrays.second]))
+        p.values <- sapply (seq (num.genes), function (g)
+            t.test.p.value (exprs.genes[g,arrays.first], exprs.genes[g,arrays.second]))
 
         return (data.frame (
-            fold.change = rowMeans (exprs.genes[,arrays.first]) - rowMeans (exprs.genes[,arrays.second]),
+            fold.change = rowMeans (exprs.genes[,arrays.first]) -
+                rowMeans (exprs.genes[,arrays.second]),
             p.value = p.values,
-            var = apply (exprs.genes[,arrays.first], 1, var) + apply (exprs.genes[,arrays.second], 1, var),
+            var = apply (exprs.genes[,arrays.first], 1, var) +
+                apply (exprs.genes[,arrays.second], 1, var),
             row.names = featureNames (batch)))
     }
 
@@ -69,7 +77,9 @@ summarize.mnf <- function (batch, summary.stat = mean, verbose = TRUE) {
         apply (exprs.probes[ps,], 2, summary.stat))))
 }
 
-normalize.mnf <- function (batch, features.i, features.b, res.pre = NULL, dolog = TRUE, doexp = FALSE, verbose = TRUE, ...) {
+normalize.mnf <- function (batch, features.i, features.b, res.pre = NULL,
+    dolog = TRUE, doexp = FALSE, verbose = TRUE, ...)
+{
     e <- exprs (batch)
     indices <- indexProbes (batch, which = "pm")
 
@@ -93,7 +103,9 @@ normalize.mnf <- function (batch, features.i, features.b, res.pre = NULL, dolog 
         # Make sure non-pm probes are not considered grid neighbours.
         features.b.pm <- features.b
         features.b.pm[is.na (res),] <- NA
-        return (normalizeChannel (e[,a], features.i = features.i, features.b = features.b.pm, res = res, verbose = verbose, ...))
+        return (normalizeChannel (e[,a], features.i = features.i,
+            features.b = features.b.pm, res = res, verbose = verbose, ...)
+        )
     }
 
     if (!is.null (features.b)) {
@@ -126,7 +138,9 @@ residuals.mnf.replicate <- function (values, samples) {
 }
 
 # TODO: Refactor normalize.mnf + normalizeChannel (into one function?)
-normalizeChannel <- function (channel, features.i, features.b, ki = 2, kb = 20, summary.stat.i = mean, summary.stat.b = mean, res = NULL, verbose = TRUE) {
+normalizeChannel <- function (channel, features.i, features.b, ki = 2, kb = 20,
+    summary.stat.i = mean, summary.stat.b = mean, res = NULL, verbose = TRUE)
+{
     if (!is.vector (channel) && !(is.matrix (channel) && ncol (channel) == 1))
         stop ("'channel' must be a vector or a 1-column matrix")
     # For some reason, rowMeans and variants do not work on 1-column matrices
@@ -146,7 +160,9 @@ normalizeChannel <- function (channel, features.i, features.b, ki = 2, kb = 20, 
     if (verbose)
         cat ("    Correcting values...\n")
     ncells <- as.integer (prod (dim (neighbours)))
-    res.mapped <- matrix (.C ("map_values", ncells, as.integer (neighbours), as.double (res), as.double (rep (NA, ncells)), NAOK = TRUE, DUP = FALSE) [[4]] , nrow = length (res))
+    res.mapped <- matrix (.C ("map_values", ncells, as.integer (neighbours),
+        as.double (res), as.double (rep (NA, ncells)), NAOK = TRUE, DUP = FALSE
+    ) [[4]], nrow = length (res))
 
     b <- !is.na (res)
     channel[b] <- channel[b] - apply (res.mapped[b,], 1, summary.stat.b)
@@ -159,7 +175,9 @@ residuals.mnf <- function (channel, pos, k, summary.stat) {
 
     neighbours <- knn.mnf (pos, k)
     ncells <- as.integer (prod (dim (neighbours)))
-    mappedVals <- .C ("map_values", ncells, as.integer (neighbours), as.double (channel), as.double (rep (NA, ncells)), NAOK = TRUE, DUP = FALSE) [[4]]
+    mappedVals <- .C ("map_values", ncells, as.integer (neighbours),
+        as.double (channel), as.double (rep (NA, ncells)), NAOK = TRUE, DUP = FALSE
+    ) [[4]]
     channel - apply (matrix (mappedVals, nrow = length (channel)), 1, summary.stat)
 }
 
@@ -174,30 +192,44 @@ knn.mnf <- function (v, k, ...) {
 
 knn.mnf.1D <- function (x, k) {
     n <- length (x)
-    matrix (.C ("array_neighbours", as.integer (n), as.integer (x), as.integer (k), as.integer (rep (NA, n * k)), NAOK = TRUE, DUP = FALSE) [[4]], nrow = n, ncol = k, byrow = TRUE)
+    matrix (.C ("array_neighbours", as.integer (n), as.integer (x), as.integer (k),
+        as.integer (rep (NA, n * k)), NAOK = TRUE, DUP = FALSE
+    ) [[4]], nrow = n, ncol = k, byrow = TRUE)
 }
 
 knn.mnf.2D <- function (x, y, k) {
     n <- length (x)
-    matrix (.C ("grid_neighbours", as.integer (n), as.integer (x), as.integer (y), as.integer (k), as.integer (rep (NA, n * k)), NAOK = TRUE, DUP = FALSE) [[5]], nrow = n, ncol = k, byrow = TRUE)
+    matrix (.C ("grid_neighbours", as.integer (n), as.integer (x), as.integer (y),
+        as.integer (k), as.integer (rep (NA, n * k)), NAOK = TRUE, DUP = FALSE
+    ) [[5]], nrow = n, ncol = k, byrow = TRUE)
 }
 
 # TODO: 'image.mnf.repres' and 'image.mnf.psres' can probably be combined
-image.mnf.repres <- function (batch, samples, which = 1:length (batch), transfo = log2, shuffle = FALSE, col = pseudoPalette (low = "blue", high = "red", mid = "white"), ...) {
+image.mnf.repres <- function (batch, samples, which = 1:length (batch),
+    transfo = log2, shuffle = FALSE, col = pseudoPalette (
+        low = "blue", high = "red", mid = "white"
+    ), ...)
+{
     num.probes <- nrow (exprs (batch))
     num.samples <- length (unique (samples))
 
     if (is.function (transfo))
         pm (batch) <- transfo (pm (batch))
     batch.res <- batch
-    exprs (batch.res) <- matrix (NA, ncol = ncol (exprs (batch)), nrow = nrow (exprs (batch)))
+    exprs (batch.res) <- matrix (NA, ncol = ncol (exprs (batch)),
+        nrow = nrow (exprs (batch))
+    )
 
     pm (batch.res) <- residuals.mnf.replicate (pm (batch), samples)
     image (batch.res[,which], transfo = NULL, col = col, ...)
     return (summary (pm (batch.res)))
 }
 
-image.mnf.psres <- function (batch, which = 1:length (batch), transfo = log2, shuffle = FALSE, cutoff = 0.5, col = pseudoPalette (low = "blue", high = "red", mid = "white"), ...) {
+image.mnf.psres <- function (batch, which = 1:length (batch), transfo = log2, 
+    shuffle = FALSE, cutoff = 0.5, col = pseudoPalette (
+        low = "blue", high = "red", mid = "white"
+    ), ...)
+{
     library (affyPLM)
 
     if (is.function (transfo))
