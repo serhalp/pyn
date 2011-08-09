9f790a1f26318f02ace905a7b3c7c82a64019d81
Philippe Serhal <philippe.serhal@umontreal.ca>
2011-08-09 19:56:43 -0400

    Major refactoring.

--- R/mnf.R
+++ R/mnf.R
@@ -4,105 +4,77 @@ library (affy)
 # FIXME: Path here should be relative to package root.
 dyn.load (paste ("~/workspace/rmnf/src/mnf_funcs", .Platform$dynlib.ext, sep = ""))
 
-mnf <- function (batch, samples, interest = "probeset", bias = "grid",
-                 features.i = NULL, features.b = NULL, verbose = TRUE, ...) {
+# TODO: Consider default value of 'do.exp'.
+normalize.mnf <- function (batch, interest = "probeset", bias = "spatial",
+                           features.i = NULL, features.b = NULL, ki = 2, kb = 20,
+                           summary.stat.i = mean, summary.stat.b = mean,
+                           res.pre = NULL, do.log = TRUE, do.exp = FALSE,
+                           verbose = TRUE, ...) {
+    # If no interest space provided, generate the requested one.
     if (is.null (features.i)) {
         features.i <- switch (interest,
                               probeset = NULL, # FIXME: This is not very expressive...
                               stop ("Need one of 'interest' or 'features.i'"))
     }
 
+    # If no bias space provided, generate the requested one.
     if (is.null (features.b)) {
         features.b <- switch (bias,
-                              grid = indices2xy (seq (nrow (exprs (batch))), abatch = batch),
-                              none = NULL, stop ("Need one of 'bias' or 'features.b'")
-        )
+                              spatial = indices2xy (seq (nrow (exprs (batch))), abatch = batch),
+                              none = NULL,
+                              stop ("Need one of 'bias' or 'features.b'"))
     }
 
-    # Compute a normalized intensity for each probe in each array
-    batch <- normalize.mnf (batch, features.i, features.b, verbose = verbose, ...)
+    # Validate the summary statistics.
+    use.median <- switch (summary.stat.i,
+                          mean = FALSE,
+                          median = TRUE,
+                          stop ("'summary.stat.i' must be either 'mean' or 'median'"))
 
-    # Compute a summarized gene expression value for each probeset, in each array
-    exprs.genes <- summarize.mnf (batch, verbose)
-
-    # Computes only the p-value (much faster than 't.test') for a two-sample
-    # t-test with unequal sample sizes and equal variances
-    t.test.p.value <- function (a, b) {
-        na <- length (a)
-        nb <- length (b)
-        df <- na + nb - 2
-        tstat <- ((mean (a) - mean (b)) /
-            (sqrt (((na - 1) * var (a) + (nb - 1) * var (b)) / df) *
-                sqrt (1 / na + 1 / nb)
-            )
-        )
-        return (2 * pt (-abs (tstat), df))
-    }
-
-    # Returns a data frame with replicate variance, log FC and t-test p-value for each gene
-    num.genes <- length (featureNames (batch))
-    diff.expr.stats <- function (pair) {
-        arrays.first <- which (samples == pair[1])
-        arrays.second <- which (samples == pair[2])
-
-        # Compute p-value of two-sample t-test, for each gene
-        p.values <- sapply (seq (num.genes), function (g)
-            t.test.p.value (exprs.genes[g, arrays.first], exprs.genes[g, arrays.second]))
-
-        return (data.frame (
-            fold.change = rowMeans (exprs.genes[, arrays.first]) -
-                rowMeans (exprs.genes[, arrays.second]),
-            p.value = p.values,
-            var = apply (exprs.genes[, arrays.first], 1, var) +
-                apply (exprs.genes[, arrays.second], 1, var),
-            row.names = featureNames (batch)))
-    }
-
-    # Compute some stats for each pair of samples
-    if (verbose)
-        cat ("Computing differential expression...\n")
-    num.samples <- length (unique (samples))
-    return (lapply (combn (num.samples, 2, simplify = FALSE), diff.expr.stats))
-}
-
-summarize.mnf <- function (batch, summary.stat = mean, verbose = TRUE) {
-    if (verbose)
-        cat ("Summarizing probeset intensities...\n")
-    exprs.probes <- exprs (batch)
-    # TODO: (Maybe) return ExpressionSet/eSet here rather than just a matrix.
-    return (t (sapply (indexProbes (batch, which = "pm"),
-                       function (ps) apply (exprs.probes[ps, ], 2, summary.stat))))
-}
-
-normalize.mnf <- function (batch, features.i, features.b, res.pre = NULL,
-                           dolog = TRUE, doexp = FALSE, use.median = FALSE,
-                           verbose = TRUE, ...) {
     e <- exprs (batch)
     indices <- indexProbes (batch, which = "pm")
 
-    if (dolog)
+    if (do.log)
         e <- log2 (e)
 
     normalize.mnf.array <- function (a) {
         if (verbose)
-            cat ("Normalizing array ", a, "...\n", sep = "")
+            cat ("Normalizing array ", a, " of ", length (batch), "...\n", sep = "")
 
-        if (is.null (features.i)) {
-            if (is.null (res.pre))
-                res <- residuals.mnf.probeset (e[, a], indices, use.median)
-            else
-                res <- res.pre[, a]
+        e.a <- e[, a]
+
+        # TODO: Refactor handling of bias/interest/features, to avoid nonsense like this.
+        if (is.null (res.pre)) {
+            if (is.null (features.i)) {
+                if (verbose)
+                    cat ("    Computing residuals...\n")
+                res <- residuals.mnf.probeset (e.a, indices, use.median)
+            } else {
+                stop ("'probeset' is currently the only valid value for 'interest'.")
+            }
         } else {
-            # TODO: Refactor handling of bias/interest/features, to avoid nonsense like this.
-            stop ("'probeset' is currently the only valid value for 'interest'.")
+            res <- res.pre[, a]
         }
 
         # Make sure non-pm probes are not considered grid neighbours.
         features.b.pm <- features.b
         features.b.pm[is.na (res), ] <- NA
-        return (normalizeChannel (e[, a], features.i = features.i,
-                                  features.b = features.b.pm, res = res,
-                                  verbose = verbose, ...))
+
+        if (verbose)
+            cat ("    Locating neighbours in bias space...\n")
+        neighbours <- knn.mnf (features.b, as.integer (kb))
+
+        if (verbose)
+            cat ("    Correcting values...\n")
+        ncells <- as.integer (prod (dim (neighbours)))
+        res.mapped <- matrix (.C ("map_values", ncells, as.integer (neighbours),
+                                  as.double (res), as.double (rep (NA, ncells)),
+                                  NAOK = TRUE, DUP = FALSE) [[4]],
+                              nrow = length (res))
+
+        b <- !is.na (res)
+        e.a[b] <- e.a[b] - apply (res.mapped[b, , drop = FALSE], 1, summary.stat.b)
+        return (e.a)
     }
 
     if (!is.null (features.b)) {
@@ -111,9 +83,10 @@ normalize.mnf <- function (batch, features.i, features.b, res.pre = NULL,
         dimnames.bak <- dimnames (e)
         e <- sapply (seq (ncol (e)), normalize.mnf.array)
         dimnames (e) <- dimnames.bak
-    }
+    } else {
+        warning ("Warning: not actually normalizing arrays, as no bias space was specified.")
 
-    if (doexp)
+    if (do.exp)
         e <- 2 ^ e
 
     exprs (batch) <- e
@@ -132,48 +105,6 @@ residuals.mnf.replicate <- function (values, samples) {
     }
 
     return (do.call (cbind, lapply (unique (samples), residuals.mnf.replicate.sample)))
-}
-
-# TODO: Refactor normalize.mnf + normalizeChannel (into one function?)
-normalizeChannel <- function (channel, features.i, features.b, ki = 2, kb = 20,
-                              summary.stat.i = mean, summary.stat.b = mean,
-                              res = NULL, verbose = TRUE) {
-    if (!is.vector (channel) && !(is.matrix (channel) && ncol (channel) == 1))
-        stop ("'channel' must be a vector or a 1-column matrix")
-
-    if (is.null (res)) {
-        if (verbose)
-            cat ("    Computing residuals...\n")
-        res <- residuals.mnf (channel, features.i, as.integer (ki), summary.stat.i)
-    }
-
-    if (verbose)
-        cat ("    Locating neighbours in bias space...\n")
-    neighbours <- knn.mnf (features.b, as.integer (kb))
-
-    if (verbose)
-        cat ("    Correcting values...\n")
-    ncells <- as.integer (prod (dim (neighbours)))
-    res.mapped <- matrix (.C ("map_values", ncells, as.integer (neighbours),
-                              as.double (res), as.double (rep (NA, ncells)),
-                              NAOK = TRUE, DUP = FALSE) [[4]],
-                          nrow = length (res))
-
-    b <- !is.na (res)
-    channel[b] <- channel[b] - apply (res.mapped[b, , drop = FALSE], 1, summary.stat.b)
-    return (channel)
-}
-
-residuals.mnf <- function (channel, pos, k, summary.stat) {
-    if (is.null (pos))
-        return (channel)
-
-    neighbours <- knn.mnf (pos, k)
-    ncells <- as.integer (prod (dim (neighbours)))
-    mappedVals <- .C ("map_values", ncells, as.integer (neighbours),
-                      as.double (channel), as.double (rep (NA, ncells)),
-                      NAOK = TRUE, DUP = FALSE) [[4]]
-    channel - apply (matrix (mappedVals, nrow = length (channel)), 1, summary.stat)
 }
 
 knn.mnf <- function (v, k, ...) {
